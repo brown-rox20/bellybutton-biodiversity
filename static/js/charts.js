17438e53cfc7f5330ccfa86c7387326484d3ccc6
Roxane Brown <roxahumada83@gmail.com>
2022-03-19 17:18:36 -0700

    add styles to index.html, static folder

--- static/js/charts.js
+++ static/js/charts.js
@@ -1,10 +1,9 @@
 function init() {
-  console.log("I got here!!!");
   // Grab a reference to the dropdown select element
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -29,7 +28,7 @@ function optionChanged(newSample) {
 
 // Demographics Panel
 function buildMetadata(sample) {
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     var metadata = data.metadata;
     // Filter the data for the object with the desired sample number
     var resultArray = metadata.filter((sampleObj) => sampleObj.id == sample);
@@ -52,7 +51,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file
-  d3.json("samples.json").then((data) => {
+  d3.json("static/data/samples.json").then((data) => {
     // 3. Create a variable that holds the samples array.
     var samples = data.samples;
 
